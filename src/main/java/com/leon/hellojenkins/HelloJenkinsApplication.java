b3af9b276a9553612f0206c8c4c2ddec21abba04
OceanLeonAI <beyondjayfir@126.com>
2021-05-23 21:06:27 +0800

    修改当前项目为 war 包部署

--- src/main/java/com/leon/hellojenkins/HelloJenkinsApplication.java
+++ src/main/java/com/leon/hellojenkins/HelloJenkinsApplication.java
@@ -2,10 +2,32 @@ package com.leon.hellojenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloJenkinsApplication {
+public class HelloJenkinsApplication extends SpringBootServletInitializer {
 
+
+//    /**
+//     * jar 方式启动
+//     *
+//     * @param args
+//     */
+//    public static void main(String[] args) {
+//        SpringApplication.run(HelloJenkinsApplication.class, args);
+//    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
+
+    /**
+     * war 启动
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(HelloJenkinsApplication.class, args);
     }
